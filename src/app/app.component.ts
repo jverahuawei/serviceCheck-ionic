1d9fb81bccb1a2d197c6f44a0f7fd99888a2155e
jose vera <jose.vera@huawei.com>
2020-05-04 12:34:30 -0500

    init commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+declare var cordova;
+
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,14 +11,56 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  hms: boolean;
+  gms: boolean;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl: AlertController) {
+    platform.ready().then(async () => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-    });
+      this.checkservices();
+    });  
   }
+
+  async checkservices() {
+    this.hms = await cordova.plugins.CordovaServiceChecker.isHmsAvailable();
+    this.gms = await cordova.plugins.CordovaServiceChecker.isGmsAvailable();
+
+    if (this.hms && this.gms){
+      const alert = this.alertCtrl.create({
+        title: 'Available Services',
+        subTitle: 'this phone has Google Services and Huawei Services Installed',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    if (this.hms && !this.gms){
+      const alert = this.alertCtrl.create({
+        title: 'Available Services',
+        subTitle: 'this phone has only Huawei Services Installed',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    if (!this.hms && this.gms){
+      const alert = this.alertCtrl.create({
+        title: 'Available Services',
+        subTitle: 'this phone has only Google Services Installed',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    if (!this.hms && !this.gms){
+      const alert = this.alertCtrl.create({
+        title: 'Available Services',
+        subTitle: 'any Services have been detected',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+  }
+
 }
 
